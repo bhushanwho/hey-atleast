0ee8ba951017611d7ed1af2666e0e4259f5bac53
bhushanwho <76513310+bhushanwho@users.noreply.github.com>
2025-09-14 16:46:51 +0530

    fixed barebones

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,11 @@ import './App.css'
 import AnimatedParagraph from './AnimatedParagraph'
 
 function App() {
-  const headingRef = useRef(null)
-  const paraRef = useRef(null)
-
-  useEffect(() => {
-    function syncWidth() {
-      if (headingRef.current && paraRef.current) {
-        paraRef.current.style.maxWidth = headingRef.current.offsetWidth + 'px'
-      }
-    }
-    syncWidth()
-    window.addEventListener('resize', syncWidth)
-    return () => window.removeEventListener('resize', syncWidth)
-  }, [])
 
   return (
     <div className="container">
-      <h1 ref={headingRef}>hey, at least</h1>
-      <AnimatedParagraph 
-        ref={paraRef}
-        text="are a cute little monkey boy hehehe i'm so cute but what happens if it extends" 
-      />
+      <h1> hey,<br></br> at least</h1>
+      <p> aret if this becomes more what i should pluck, it'll become very sad</p>
     </div>
   )
 }
